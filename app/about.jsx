8782d73506db11e235a02306e140e1f0fb4a5ce4
Piyush <rathorepiyush0000@gmail.com>
2025-12-01 21:44:33 +0530

    setup done

---
+++ app/about.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Muscle Matrix App!</Text>
+    </View>
+  );
+}
+
